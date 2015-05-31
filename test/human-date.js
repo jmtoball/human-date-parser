613d930fad7fb978d76fc10aab42478b018bdcf1
Johannes Maximilian Toball <mtoball@crealytics.de>
2015-06-01 01:04:17 +0200

    DRY tests

--- test/human-date.js
+++ test/human-date.js
@@ -1,57 +1,52 @@
 var expect = require('chai').expect,
     moment = require('moment'),
-    humanDate = require('../human-date.js');
+    humanDate = require('../human-date.js'),
+    inputs = require('./inputs.json');
 
 var now = moment("2015-01-15T09");
 
-function diff(dateString, unit){
-  return moment(humanDate.parse(dateString, now)).diff(now, unit);
+function getDateFragment(date, unit) {
+  unit = unit.replace(/s$/, '');
+  if (["year", "month", "week", "day", "hour", "minute", "second"].indexOf(unit) >= 0) {
+    if (unit == "day") {
+      unit = "date";
+    }
+    return date[unit]();
+  } else {
+    throw 'Unsuspported unit ' + unit;
+  }
+}
+function applyInput(input, outputs) {
+  it('should turn ' + input + ' into ' + JSON.stringify(outputs), function () {
+    var result = humanDate.parse(input, now);
+    var unit, output, value, diff;
+    if (typeof outputs.relative !== 'undefined') {
+      output = outputs.relative;
+      for (unit in output) {
+        value = output[unit];
+        diff = getDateFragment(result, unit) - getDateFragment(now, unit);
+        expect(diff).to.equal(value);
+      }
+    }
+    if (typeof outputs.absolute !== 'undefined') {
+      output = outputs.absolute;
+      for (unit in output) {
+        value = output[unit];
+        expect(getDateFragment(result, unit)).to.equal(value);
+      }
+    }
+  });
+}
+
+function applyInputs(inputs, method) {
+  for (var input in inputs[method]) {
+    var outputs = inputs[method][input];
+    applyInput(input, outputs);
+  }
 }
 
 describe('#parse', function () {
-
-  it('should recognize simple date aliases', function () {
-    expect(diff("tomorrow", "days")).to.equal(1);
-    expect(diff("the day after tomorrow", "days")).to.equal(2);
-    expect(diff("yesterday", "days")).to.equal(-1);
-    expect(diff("the day before yesterday", "days")).to.equal(-2);
-    expect(diff("last year", "years")).to.equal(-1);
-    expect(diff("next year", "years")).to.equal(1);
-  });
-
-  it('should recognize relative weekdays', function () {
-    expect(diff("next friday", "days")).to.equal(1);
-    expect(diff("last thursday", "days")).to.equal(-7);
-    expect(diff("last monday next week", "days")).to.equal(4);
-    expect(diff("friday next week", "days")).to.equal(8);
-    expect(diff("tomorrow next week", "days")).to.equal(8);
-    expect(diff("friday last week", "days")).to.equal(-6);
-    expect(diff("the day before friday last week", "days")).to.equal(-7);
-    expect(diff("the day after friday last week", "days")).to.equal(-5);
-  });
-
-  it('should recognize combined date and time aliases', function () {
-    var tomorrowAtNoon = humanDate.parse("tomorrow at noon", now);
-    expect(tomorrowAtNoon.hours()).to.equal(12);
-    expect(tomorrowAtNoon.date()).to.equal(16);
-
-    var yesterdayAtNoon = humanDate.parse("yesterday at noon", now);
-    expect(yesterdayAtNoon.hours()).to.equal(12);
-    expect(yesterdayAtNoon.date()).to.equal(14);
-
-    var tomorrowAtMidnight = humanDate.parse("tomorrow at midnight", now);
-    expect(tomorrowAtMidnight.hours()).to.equal(0);
-    expect(tomorrowAtMidnight.date()).to.equal(17);
-
-    var lastYearsTomorrowAtMidnight = humanDate.parse("tomorrow last year at midnight", now);
-    expect(lastYearsTomorrowAtMidnight.hours()).to.equal(0);
-    expect(lastYearsTomorrowAtMidnight.date()).to.equal(17);
-    expect(lastYearsTomorrowAtMidnight.year()).to.equal(2014);
-
-    var lastYearsTomorrowAtMidnight = humanDate.parse("yesterday next year at 8:00", now);
-    expect(lastYearsTomorrowAtMidnight.hours()).to.equal(8);
-    expect(lastYearsTomorrowAtMidnight.date()).to.equal(14);
-    expect(lastYearsTomorrowAtMidnight.year()).to.equal(2016);
-  });
-
+  applyInputs(inputs, "parseDate");
+  applyInputs(inputs, "parseTime");
+  applyInputs(inputs, "parse");
 });
